7c4ebf1fa964137cf417efc29568b390ae661b54
Narmatha-Rp <20d226@psgtech.ac.in>
2023-09-06 06:13:20 +0530

    created python file

--- file2.py
+++ file2.py
@@ -1,15 +1,19 @@
 
-def swapList(newList):
-	size = len(newList)
-	
-	# Swapping
-	temp = newList[0]
-	newList[0] = newList[size - 1]
-	newList[size - 1] = temp
-	
-	return newList
+# Python program to find the
+# maximum of two numbers
 
-newList = [12, 35, 9, 56, 24]
 
-print(swapList(newList))
+def maximum(a, b):
+	
+	if a >= b:
+		return a
+	else:
+		return b
+	
+# Driver code
+a = 20
+b = 4
+print(maximum(a, b))
+
+
 
